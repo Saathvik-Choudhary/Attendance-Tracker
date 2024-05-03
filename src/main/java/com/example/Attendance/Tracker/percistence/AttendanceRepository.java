06655d938a7a72ce07deed2e3533bd1ca0b89a8c
Zero-definition <saathvikchoudhary@gmail.com>
2024-05-03 13:16:44 +0530

    repo function

--- src/main/java/com/example/Attendance/Tracker/percistence/AttendanceRepository.java
+++ src/main/java/com/example/Attendance/Tracker/percistence/AttendanceRepository.java
@@ -2,8 +2,14 @@ package com.example.Attendance.Tracker.percistence;
 
 import com.example.Attendance.Tracker.domain.Attendance;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AttendanceRepository extends JpaRepository<Attendance,Long> {
+
+    @Query("SELECT SUM(e.workingHours) "
+            + "FROM Attendance e "
+            + "WHERE e.dateid = ?1")
+    int getMonthSummary(int dateid);
 }
